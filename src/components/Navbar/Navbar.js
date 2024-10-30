5946ce28e3b741c70077c2ceeaf477354080ba59
biogolf10457 <biogolf10457@gmail.com>
2024-10-30 18:18:48 +0700

    make navbar to responsive and hamburger menu function for small devices

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-scroll';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className='navbar'>
-            <div className='navLogo'>
-                Logo
+            <div className='navTitle'>
+                Portfolio
             </div>
-            <div className='navMenu'>
+            <div className={`${'navHamburger'} ${menuOpen && 'navHamburgerOpen'}`}
+            onClick={() => setMenuOpen(!menuOpen)}>
+                    <div className='hamburgerBar'></div>
+                    <div className='hamburgerBar'></div>
+                    <div className='hamburgerBar'></div>
+            </div>
+            <div className={`${'navMenu'} ${menuOpen && 'navMenuOpen'}`}>
                 <Link className='navMenuItem'>Home</Link>
                 <Link className='navMenuItem'>Work</Link>
                 <Link className='navMenuItem'>Project</Link>
